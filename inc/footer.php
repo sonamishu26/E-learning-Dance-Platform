a08a7b6ecc50ca258e47928939522a461a8966de
sonamishu26 <shwetabala81@gmail.com>
2025-12-02 21:04:54 +0530

    first

---
+++ inc/footer.php
@@ -0,0 +1,21 @@
+
+    <!-- <div class="footer">
+  <center> <strong><a href="https://github.com/DubeyJi0840" target="_blank">Move With Us</a></strong></center>
+    </div> -->
+  
+   <div>
+
+
+
+
+<footer class="footer">
+  <p>
+    <span style="font-weight: bold;">Contact:</span> +91-8888888888 <br>
+    <span style="font-weight: bold;">E-mail:</span> Adi13388@gmail.com <br>
+    <span style="font-weight: bold;">Address:</span> Patna, Bihar<br>
+    <strong><a href="https://github.com/DubeyJi0840" target="_blank"><center><h3>Move With Us</h3></center></a></strong>
+  </p>
+</footer>
+</div>
+
+
